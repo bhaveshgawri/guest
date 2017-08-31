7671051c7b45b7fb93a2b2926854f30a24db0b6b
Bhavesh Gawri <4bhaveshgawri@gmail.com>
2017-09-01 00:41:36 +0530

    add command line arguments

--- guest/guest.py
+++ guest/guest.py
@@ -1,4 +1,30 @@
+#!/usr/bin/python
+import argparse
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
+parser = argparse.ArgumentParser(description=
+								"""Open [proxysite.com] 
+								or [images.google.com].
+								<-Opens google.com by 
+								default-> Use only one
+								argument.""")
+parser.add_argument('-p', action='store_true',
+					dest='proxy',
+					help='Open with proxysite.com')
+				
+parser.add_argument('-i', action='store_true',
+					dest='images',
+					help='Open with images.google.com')
+
+results = parser.parse_args()
 driver = webdriver.Chrome()
-driver.get("https://www.proxysite.com/")
+	
+if results.images and results.proxy:
+	driver.get("https://www.google.com")
+elif results.images:
+	driver.get("https://www.images.google.com")
+elif results.proxy:
+	driver.get("https://www.proxysite.com/")
+else:
+	driver.get("https://www.google.com")
